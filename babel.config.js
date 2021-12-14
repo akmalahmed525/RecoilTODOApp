a7db096baad4e89583d7c68d6dc65cfd592420bb
Ahmed Akmal <akmal.ahmed525@gmail.com>
2021-12-15 02:28:02 +0530

    implemented absolute imports

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@src': ['./src'],
+        },
+      },
+    ],
+  ],
 };
---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import React, {FC} from 'react';
+
+type AppProps = {};
+const App: FC<AppProps> = () => <></>;
+
+export default App;
